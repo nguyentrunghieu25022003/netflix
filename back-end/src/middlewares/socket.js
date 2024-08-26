f3401108657ea89deecb5663300086211288191d
unknown <trunghieu20032502@gmail.com>
2024-08-26 11:04:27 +0700

    update commit

--- back-end/src/middlewares/socket.js
+++ back-end/src/middlewares/socket.js
@@ -3,7 +3,20 @@ const socket = require("socket.io");
 const initSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: process.env.CLIENT_URL,
+      origin: function (origin, callback) {
+        if (!origin) {
+          return callback(null, true);
+        }
+        if (process.env.NODE_ENV === "production") {
+          if (origin === process.env.CLIENT_URL) {
+            callback(null, true);
+          } else {
+            callback(new Error("Not allowed by CORS"));
+          }
+        } else {
+          callback(null, true);
+        }
+      },
       credentials: true,
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
       allowedHeaders: ["Authorization", "Content-Type"],
