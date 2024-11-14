53fa432ef509f24208d18ca4b4317965121d5b61
unknown <trunghieu20032502@gmail.com>
2024-11-14 14:07:57 +0700

    update commit

--- front-end/src/components/thank/thank.jsx
+++ front-end/src/components/thank/thank.jsx
@@ -9,12 +9,12 @@ const Thank = () => {
       <div className="container">
         <div className="row">
           <div className="col-xl-12">
-            <h4>Thank you for&nbsp;choosing Netflix</h4>
+            <h4>Thank you for&nbsp;choosing Fakeflix</h4>
           </div>
         </div>
         <div className="row pt-5">
           <div className="col-xl-6 col-lg-6 col-md-12 col-12">
-            <img src={"/assets/imgs/Netflix-PPT-Template.jpg"} alt="error" />
+            <img src={"/assets/imgs/68747470733a2f2f63646e2e6a7364656c6976722e6e65742f67682f54683357616c6c2f6173736574732d63646e2f46616b65666c69782f46616b65666c69785f726561646d652e706e67.png"} alt="error" />
           </div>
           <div className={cx("col-xl-6 desc")}>
             <p>
